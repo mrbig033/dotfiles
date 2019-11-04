6571e611690229530f6da3fd032e55f147002cfa
mrbig033 <mrbig033@protonmail.com>
2019-11-04 19:15:01 -0300

    add py scripts for locking and unlocking emacs

--- scripts/python/chrome_to_emacs.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import pyautogui
-
-pyautogui.hotkey
-
-pyautogui.hotkey('ctrl', 'a')
---
+++ scripts/python/unlock-emacs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+""" Locks Emacs """
+
+import os
+import getpass
+
+os.system("clear")
+
+ANSWER = ""
+
+PASSWORD = "os pais trocaram algumas palavras acerbas"
+
+while ANSWER != PASSWORD:
+    ANSWER = getpass.getpass("\n Please type the correct password:\n ")
+
+os.system("sudo chmod 775 ~/e/init.el")
+os.system("sudo chmod 775 ~/e/init.org")
+os.system("sudo chmod 775 ~/scripts/python/lock-emacs.py")
+os.system("sudo chmod 775 ~/scripts/python/unlock-emacs.py")
+
+print("\n Emacs Unlocked\n")
+
+exit()
