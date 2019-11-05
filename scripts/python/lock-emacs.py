bbe8dcdf93ef8fd9e2c3f4b22cd73ec1718b9bc5
mrbig033 <mrbig033@protonmail.com>
2019-11-05 16:00:38 -0300

    fix emacs i3 key

--- scripts/python/lock-emacs.py
+++ scripts/python/lock-emacs.py
@@ -8,10 +8,10 @@ os.system("clear")
 
 input("\n Press Enter to lock Emacs\n ")
 
-os.system("sudo chmod 000 ~/e/init.el")
-os.system("sudo chmod 000 ~/e/init.org")
-os.system("sudo chmod 450 ~/scripts/python/lock-emacs.py")
-os.system("sudo chmod 450 ~/scripts/python/unlock-emacs.py")
+os.system("sudo chmod 445 ~/e/init.el")
+os.system("sudo chmod 445 ~/e/init.org")
+os.system("sudo chmod 555 ~/scripts/python/lock-emacs.py")
+os.system("sudo chmod 555 ~/scripts/python/unlock-emacs.py")
 
 print("\n Emacs Locked\n")
 
---
+++ scripts/python/map.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+time_minutes = int(input("\n Please type the time desire in minutes."))
+time_seconds = time_minutes * 60
+e
