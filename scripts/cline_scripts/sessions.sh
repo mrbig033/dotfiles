34c626bfd4ffaa62038dcd8a9765315e74991192
mrbig033 <mrbig033@protonmail.com>
2019-11-05 18:29:56 -0300

    fucking fixed term up i3 scratchs+tmux

--- scripts/cline_scripts/sessions.sh
+++ scripts/cline_scripts/sessions.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-tmux kill-server && source "$HOME"/.bash_aliases && tmux new-session -ds 1
-tmux new-session -ds 2
-tmux new-session -ds 3
-tmux new-session -ds 4
-tmux new-session -ds 5
-tmux new-session -ds 6
-tmux new-session -ds 7
-tmux new-session -ds 8
-tmux new-session -ds 9
-tmux new-session -ds 0
-# tmux attach -t 0 &>/dev/null || tmux switch-client -t 0 && xfce4-terminal --working-directory=~ --title=term-up --hold --command="tmux attach" &
+source "$HOME"/.bash_aliases
+tmux new-session -ds 1
+tmux new-session -ds 2 
+tmux new-session -ds 3 
+tmux new-session -ds 4 
+tmux new-session -ds 5 
+tmux new-session -ds 6 
+tmux new-session -ds 7 
+tmux new-session -ds 8 
+tmux new-session -ds 9 
+tmux new-session -ds 0 
+notify-send --expire-time=250 'tmux reloaded'
+i3-msg "[title=term-up]" scratchpad show
 # vim: ft=tmux nowrap
