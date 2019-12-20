bcecdf42396964bb58e2ab56c2b8ac0357dc5b58
mrbig033 <mrbig033@protonmail.com>
2019-12-20 03:43:58 -0300

    scripts/python/po.py

--- scripts/python/po.py
+++ scripts/python/po.py
@@ -2,25 +2,28 @@
 
 import sys
 from os import system
+from os import environ
 from random import randint
 from datetime import datetime
 
 system("clear")
 sys.tracebacklimit = 0
 
-time = datetime.now()
-time_str = time.strftime("%d/%m/%y, %H:%M:%S")
+path = f"{environ['HOME']}/.po.csv"
 
 
 def choose_poetry():
+    time = datetime.now()
+    time_str = time.strftime("%d/%m/%y, %H:%M")
     vol = randint(1, 2)
     vol1 = randint(1, 1120)
     vol2 = randint(1, 411)
     if vol == 1:
-        return f"Volume {vol}, Poesia {vol1}"
+        return f"{time_str}, Volume {vol}, Poesia {vol1}\n"
 
     else:
-        return f"Volume {vol}, Poesia {vol2}"
+        return f"{time_str}, Volume {vol}, Poesia {vol2}\n"
 
 
-choose_poetry()
+with open(path, "a") as file:
+    file.write(choose_poetry())
