a69d77b95865d1c381c3c3bb49988061782fe7b6
mrbig033 <mrbig033@protonmail.com>
2019-12-20 03:30:26 -0300

    scripts/python/po.py

--- scripts/python/po.py
+++ scripts/python/po.py
@@ -2,10 +2,25 @@
 
 import sys
 from os import system
-from random import randint as r
+from random import randint
 from datetime import datetime
 
 system("clear")
 sys.tracebacklimit = 0
 
-print
+time = datetime.now()
+time_str = time.strftime("%d/%m/%y, %H:%M:%S")
+
+
+def choose_poetry():
+    vol = randint(1, 2)
+    vol1 = randint(1, 1120)
+    vol2 = randint(1, 411)
+    if vol == 1:
+        return f"Volume {vol}, Poesia {vol1}"
+
+    else:
+        return f"Volume {vol}, Poesia {vol2}"
+
+
+choose_poetry()
