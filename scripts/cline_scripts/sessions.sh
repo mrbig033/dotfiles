49b6198bfb5488f6490aa7d51d0afaedc352ddc2
mrbig033 <mrbig033@protonmail.com>
2020-02-11 21:00:12 -0300

    auto-commit

--- scripts/cline_scripts/sessions.sh
+++ scripts/cline_scripts/sessions.sh
@@ -11,7 +11,5 @@ tmux new-session -ds 7
 tmux new-session -ds 8
 tmux new-session -ds 9
 tmux new-session -ds 0
-# tmux switch-client -t ranger &>/dev/null || tmux -L ranger
-# notify-send --expire-time=250 'tmux reloaded'
-i3-msg "[title=term-up]" scratchpad show
+tmux attach -t 0 &>/dev/null || tmux switch-client -t 0
 # vim: ft=tmux nowrap
