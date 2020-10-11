c42747571841788dfae902b4eda3dcd33ff22997
mrbig033 <mrbig033@protonmail.com>
2020-10-10 21:00:12 -0300

    auto-commit

--- scripts/screen/mx-hdmi-screen.sh
+++ scripts/screen/mx-hdmi-screen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-xrandr --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --off --output HDMI-2 --off
-pkill compton
+# xrandr --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --off --output HDMI-2 --off
+# pkill compton
 compton -b --backend glx &>/dev/null
 feh --recursive --randomize --bg-fill ~/Pictures/Wallpaper
 sleep 1
